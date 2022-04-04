04193794ee2faa68ec28635a5e975533434beeec
unknown <2735720689@qq.com>
2022-04-04 14:30:29 +0800

    ''

---
+++ cloudmusic/src/base/mm-icon/mm-icon.vue
@@ -0,0 +1,22 @@
+
+<script>
+// icon组件
+export default {
+  name: 'MmIcon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: Number,
+      default: 16
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style>
+</style>
--- cloudmusic/src/components/music-list/music-list.vue
+++ cloudmusic/src/components/music-list/music-list.vue
@@ -6,24 +6,39 @@
         <span class="list-name">歌曲</span>
         <span class="list-artist">歌手</span>
         <span class="list-time" v-if="listType === 1">时长</span>
-        <span class="list-ablum" v-else>专辑</span>
+        <span class="list-album" v-else>专辑</span>
       </div>
       <div class="list-content" ref="listContent" @scroll="listScroll($event)">
+        <!-- 这个div针对单独的歌曲定义一些标签和事件 -->
         <div
           v-for="(item, index) in list"
           :key="item.id"
           class="list-item"
           :class="{ on: playing && currentMusic.id === item.id }"
           @dblclick="selectItem(item, index, $event)"
-        ></div>
+        >
+          <span class="list-num" v-text="index + 1"></span>
+          <div class="list-name">
+            <span>{{ item.name }}</span>
+            <div class="list-menu">
+              <mm-icon class="hover">2022.4.4☆☆☆☆☆☆</mm-icon>
+            </div>
+          </div>
+          <span class="list-artist">{{ item - singer }}</span>
+          <span v-if="listType === 1" class="list-time"></span>
+          <span v-else class="list-album">{{ item.album }}</span>
+        </div>
+        <slot name="listBtn"></slot>
       </div>
     </template>
     <div v-else>title="弄啥嘞，怎么啥也没有！！！"</div>
   </div>
 </template>
 <script>
+import MmIcon from 'base/mm-icon/mm-icon.vue'
 export default {
   name: 'MusicList',
+  components: { MmIcon },
   data() {
     return {}
   },
